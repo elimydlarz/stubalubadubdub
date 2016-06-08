f74a12cf553181114dfb22c417ac67f5397d57a5
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-06-08 21:16:47 +0800

    Using non-deprecated way to read params.

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const mongo = mongodb.MongoClient;
 app.use('/mongoses', mongo_express(mongo_express_config));
 
 app.get('/:db/:collection', (req, res) => {
-  mongo.connect(`mongodb://localhost:27017/${req.param('db')}`, (err, db) => {
-    db.collection(req.param('collection')).find().toArray(function(err, result) {
+  mongo.connect(`mongodb://localhost:27017/${req.params.db}`, (err, db) => {
+    db.collection(req.params.collection).find().toArray(function(err, result) {
       res.json(result);
     });
   });
