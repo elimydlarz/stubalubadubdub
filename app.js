f148907fd77a94cf5eec6e1378d87c1a4c6630c6
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-06-09 16:45:47 +0800

    Refactoring.

--- app.js
+++ app.js
@@ -4,38 +4,36 @@ import mongo_express from 'mongo-express/lib/middleware';
 import mongo_express_config from './mongo.config.js';
 
 const app = express();
-const mongo = mongodb.MongoClient;
+
+const retrieveData = (databaseName, collectionName, query, respond) =>
+    mongodb.MongoClient.connect(`mongodb://localhost:27017/${databaseName}`, (err, db) =>
+      db.collection(collectionName).find(query).toArray(respond)
+    );
 
 app.use('/mongoses', mongo_express(mongo_express_config));
 
-app.get('/:db/:collection', (req, res) => {
-  mongo.connect(`mongodb://localhost:27017/${req.params.db}`, (err, db) => {
-    db.collection(req.params.collection).find().toArray(function(err, result) {
-      res.json(result);
-    });
-  });
+app.get('/:db/:collection', (request, response) => {
+  retrieveData(request.params.db, request.params.collection, undefined, (error, result) =>
+      response.json(result)
+  );
 });
 
-app.get('/:db/:collection/header/:header', (req, res) => {
-  mongo.connect(`mongodb://localhost:27017/${req.params.db}`, (err, db) => {
-    let query = {};
-    query[req.params.header] = req.header(req.params.header);
-
-    db.collection(req.params.collection).find(query).toArray(function(err, result) {
-      res.json(result);
-    });
-  });
-});
-
-app.get('/:db/:collection/key/:key/value/:value', (req, res) => {
+app.get('/:db/:collection/header/:header', (request, response) => {
   let query = {};
-  query[req.params.key] = req.header(req.params.value);
+  query[request.params.header] = request.header(request.params.header);
 
-  mongo.connect(`mongodb://localhost:27017/${req.params.db}`, (err, db) => {
-    db.collection(req.params.collection).find(query).toArray(function(err, result) {
-      res.json(result);
-    });
-  });
+  retrieveData(request.params.db, request.params.collection, query, (error, result) =>
+      response.json(result)
+  );
+});
+
+app.get('/:db/:collection/key/:key/value/:value', (request, response) => {
+  let query = {};
+  query[request.params.key] = request.params.value;
+
+  retrieveData(request.params.db, request.params.collection, query, (error, result) =>
+      response.json(result)
+  );
 });
 
 app.listen(3000);
