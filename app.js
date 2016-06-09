82d4fad4740758d39bdea600fdc9daa50b5c2c7a
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-06-09 21:30:50 +0800

    It turns out we can't use multiple databases at once with express mongo, so we are just using one.

--- app.js
+++ app.js
@@ -5,33 +5,33 @@ import mongo_express_config from './mongo.config.js';
 
 const app = express();
 
-const retrieveData = (databaseName, collectionName, query, respond) =>
-    mongodb.MongoClient.connect(`mongodb://localhost:27017/${databaseName}`, (err, db) =>
+const retrieveData = (collectionName, query, respond) =>
+    mongodb.MongoClient.connect('mongodb://localhost:27017/db', (err, db) =>
       db.collection(collectionName).find(query).toArray(respond)
     );
 
 app.use('/mongoses', mongo_express(mongo_express_config));
 
-app.get('/:db/:collection', (request, response) => {
-  retrieveData(request.params.db, request.params.collection, undefined, (error, result) =>
+app.get('/:collection', (request, response) => {
+  retrieveData(request.params.collection, undefined, (error, result) =>
       response.json(result)
   );
 });
 
-app.get('/:db/:collection/header/:header', (request, response) => {
+app.get('/:collection/header/:header', (request, response) => {
   let query = {};
   query[request.params.header] = request.header(request.params.header);
 
-  retrieveData(request.params.db, request.params.collection, query, (error, result) =>
+  retrieveData(request.params.collection, query, (error, result) =>
       response.json(result)
   );
 });
 
-app.get('/:db/:collection/key/:key/value/:value', (request, response) => {
+app.get('/:collection/key/:key/value/:value', (request, response) => {
   let query = {};
   query[request.params.key] = request.params.value;
 
-  retrieveData(request.params.db, request.params.collection, query, (error, result) =>
+  retrieveData(request.params.collection, query, (error, result) =>
       response.json(result)
   );
 });
